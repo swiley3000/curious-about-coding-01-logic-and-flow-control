52efce998b0b466770fa15cc5218a6d4efa46c85
swiley3000 <swiley3000@yahoo.co.uk>
2024-12-03 13:35:21 +0000

    initial commit when changing laptops

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from logic_and_flow_control.game import GameWindow
 def start_adventure():
     GameWindow.update(
         image_name="door.png",
-        message="You come to a wall with two doors. You go through door A or door B?",
+        message="You come to a wall with two doors. Do you want go through door A or door B?",
         option_a="Door A",
         option_b="Door B",
         next_function=door_a_or_b,
@@ -14,19 +14,34 @@ def start_adventure():
 def door_a_or_b(choice):
     if choice == "a":
         GameWindow.update(
-            image_name="question.png",
-            message="Description of scene. Description of choice?",
-            option_a="Description of option a",
-            option_b="Description of option b",
-            next_function=door_a_or_b,
+            image_name="bridge.png",
+            message="You see a bridge over a river, there is a troll on the bridge. Do you take the bridge or try to swim across?",
+            option_a="Bridge",
+            option_b="Swim",
+            next_function=bridge_choice,
         )
     else:
         GameWindow.update(
-            image_name="question.png",
-            message="Description of scene. Description of choice?",
-            option_a="Description of option a",
-            option_b="Description of option b",
-            next_function=door_a_or_b,
+            image_name="ring.png",
+            message="A spider guards the ring of power but there is a sword. Do you take the ring and run or take the sword and fight the spider?",
+            option_a="Ring",
+            option_b="Fight",
+            next_function=bridge_choice,
+        )
+
+
+def bridge_choice(choice):
+    if choice == "a":
+        GameWindow.end(
+            image_name="bridge.png",
+            message="The troll kills you, bad luck. Try again...",
+            restart_function=start_adventure,
+        )
+    else:
+        GameWindow.end(
+            image_name="bridge.png",
+            message="You drown, oh dear. Try again...",
+            restart_function=start_adventure,
         )
 
 
---
+++ variable.py
@@ -0,0 +1,20 @@
+name= "Slim Shady"
+print("Hello my is "+name)
+
+
+print()
+name= "Eminem"
+print("Hi my name is",name,"how are you?")
+print()
+
+banana= 23
+
+print("I'm good thanks, out of ten I feel",banana)
+apple=5
+print()
+print(apple+banana+0.5)
+another_name=apple
+apple=6
+print(another_name)
+print(apple)
+
